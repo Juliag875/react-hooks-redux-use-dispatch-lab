38447eae3a3757eaee3d57c4c5d3988c088656ea
Iuliia El Sammak <juliag875@gmail.com>
2022-05-10 00:52:31 -0400

    commit tree 99d9eb59864bf2ce2d1fbfc644b347c9a4f4b723

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 import App from "./App";
 import { Provider } from "react-redux";
 import { createStore } from "redux";
-
 import restaurantsReducer from "./features/restaurant/restaurantsSlice";
 
 const store = createStore(restaurantsReducer);
